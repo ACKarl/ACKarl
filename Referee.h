3440a82697beca340bdf98d8f1fb6ecfedc5431c
ACKarl <104746285+ACKarl@users.noreply.github.com>
2023-03-13 15:59:04 +0800

    Add files via upload

---
+++ Referee.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Computer.h"
+#include "Human.h"
+
+#ifndef REFEREE_H
+#define REFEREE_H
+
+class Referee{
+public:
+     Referee();
+     Player* refGame(Player* player1, Player* player2);
+};
+#endif
